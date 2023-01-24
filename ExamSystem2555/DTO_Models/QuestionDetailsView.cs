bc94a1f32277b9089ed05c06ebd08de23217bf38
George Petrou <giorgospetrougp@gmail.com>
2023-01-25 01:29:34 +0200

    some more changes

---
+++ ExamSystem2555/DTO_Models/QuestionDetailsView.cs
@@ -0,0 +1,15 @@
+﻿using MyDatabase.Models;
+
+namespace WebApp.DTO_Models
+{
+    public class QuestionDetailsView
+    {
+        public int QuestionDetailsViewId { get; set; }
+        public string QuestionDisplay { get; set; }
+        public string QuestionDifficulty { get; set; }
+        public List<QuestionPossibleAnswer> Answers { get; set; }
+        public List<Topic> Topics { get; set; }
+        public List<Certificate> Certificates { get; set; }
+
+    }
+}
