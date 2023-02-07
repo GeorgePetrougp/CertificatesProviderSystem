216b8a95369c86e7ced6bd51b0808294dede6439
Mike <chatzidiakosm@gmail.com>
2023-02-07 19:18:08 +0200

    AppRoles, CertificateExam, Questions, UserRoles layout done

--- ExamSystem2555/Models/ApplicationUser.cs
+++ ExamSystem2555/Models/ApplicationUser.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Models
 {
     public class ApplicationUser:IdentityUser
     {
+        [DisplayName("First Name")]     
         public string FirstName { get; set; }
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
 
     }
